3959c294806efc49e1a809e76f46968e46cbffe0
Byron Tasseff <btasseff@lanl.gov>
2018-05-10 17:11:44 -0600

    Correcting solver variable.

--- test/feasibility.jl
+++ test/feasibility.jl
@@ -1,36 +1,36 @@
 @testset "test minlp feasibility problems" begin
     @testset "balerma" begin
-        solution = run_feasibility("../test/data/epanet/balerma.inp", GenericWaterModel{StandardMINLPForm}, bonmin)
+        solution = run_feasibility("../test/data/epanet/balerma.inp", GenericWaterModel{StandardMINLPForm}, solver)
         @test true
     end
 
     @testset "foss_poly_1" begin
-        solution = run_feasibility("../test/data/epanet/foss_poly_1.inp", GenericWaterModel{StandardMINLPForm}, bonmin)
+        solution = run_feasibility("../test/data/epanet/foss_poly_1.inp", GenericWaterModel{StandardMINLPForm}, solver)
         @test true
     end
 
     @testset "hanoi_extended" begin
-        solution = run_feasibility("../test/data/epanet/hanoi_extended.inp", GenericWaterModel{StandardMINLPForm}, bonmin)
+        solution = run_feasibility("../test/data/epanet/hanoi_extended.inp", GenericWaterModel{StandardMINLPForm}, solver)
         @test true
     end
 
     @testset "hanoi" begin
-        solution = run_feasibility("../test/data/epanet/hanoi.inp", GenericWaterModel{StandardMINLPForm}, bonmin)
+        solution = run_feasibility("../test/data/epanet/hanoi.inp", GenericWaterModel{StandardMINLPForm}, solver)
         @test true
     end
 
     @testset "klmod" begin
-        solution = run_feasibility("../test/data/epanet/klmod.inp", GenericWaterModel{StandardMINLPForm}, bonmin)
+        solution = run_feasibility("../test/data/epanet/klmod.inp", GenericWaterModel{StandardMINLPForm}, solver)
         @test true
     end
 
     @testset "rural" begin
-        solution = run_feasibility("../test/data/epanet/rural.inp", GenericWaterModel{StandardMINLPForm}, bonmin)
+        solution = run_feasibility("../test/data/epanet/rural.inp", GenericWaterModel{StandardMINLPForm}, solver)
         @test true
     end
 
     @testset "zj" begin
-        solution = run_feasibility("../test/data/epanet/zj.inp", GenericWaterModel{StandardMINLPForm}, bonmin)
+        solution = run_feasibility("../test/data/epanet/zj.inp", GenericWaterModel{StandardMINLPForm}, solver)
         @test true
     end
 end
