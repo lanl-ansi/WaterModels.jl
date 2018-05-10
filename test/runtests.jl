c81889b02124813b27afcde3c1534121ec188768
Byron Tasseff <btasseff@lanl.gov>
2018-05-10 17:05:30 -0600

    Trying different solver.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,19 +1,17 @@
 using WaterModels
+using Base.Test
+using JuMP
 using InfrastructureModels
+using Ipopt
 using Memento
+using Pajarito
 
 # Suppress warnings during testing.
 setlevel!(getlogger(InfrastructureModels), "error")
 setlevel!(getlogger(WaterModels), "error")
 
-using JuMP, AmplNLWriter
-using Base.Test
-
 # Solver setups.
-options = ["bonmin.bb_log_level=0","bonmin.fp_log_level=0",
-           "bonmin.lp_log_level=0","bonmin.milp_log_level=0",
-           "bonmin.nlp_log_level=0","bonmin.oa_log_level=0"]
-bonmin = AmplNLSolver("bonmin", options)
+solver = PajaritoSolver(cont_solver = IpoptSolver())
 
 # Perform the tests.
 @testset "WaterModels" begin
