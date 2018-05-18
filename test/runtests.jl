3c29aedbe1bf9809a121792fc0644b24dfb55149
Byron Tasseff <btasseff@lanl.gov>
2018-05-18 10:37:15 -0600

    Reverting back to Bonmin for testing.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,6 @@
 using WaterModels
 using Base.Test
-using JuMP
+using JuMP, AmplNLWriter
 using InfrastructureModels
 using Ipopt
 using Memento
@@ -10,8 +10,11 @@ using Pajarito
 setlevel!(getlogger(InfrastructureModels), "error")
 setlevel!(getlogger(WaterModels), "error")
 
-# Solver setups.
-solver = PajaritoSolver(cont_solver = IpoptSolver())
+# Solver setup.
+options = ["bonmin.bb_log_level=0","bonmin.fp_log_level=0",
+           "bonmin.lp_log_level=0","bonmin.milp_log_level=0",
+           "bonmin.nlp_log_level=0","bonmin.oa_log_level=0"]
+solver = AmplNLSolver("bonmin", options)
 
 # Perform the tests.
 @testset "WaterModels" begin
