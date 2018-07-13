1f64674e2f723cd708667ab3c81d48050558777a
Byron Tasseff <btasseff@lanl.gov>
2018-07-13 09:59:53 -0600

    Fixes #6.

--- src/WaterModels.jl
+++ src/WaterModels.jl
@@ -11,12 +11,8 @@ using Memento
 
 import Compat: @__MODULE__
 
-# Create the module level logger. (This will get precompiled.)
+# Create and initialize the module level logger.
 const LOGGER = getlogger(@__MODULE__)
-setlevel!(LOGGER, "info")
-
-# Register the module level logger at runtime so the logger can be accessed via `getlogger(WaterModels)`
-# NOTE: If this line is not included, then the precompiled `WaterModels.LOGGER` won't be registered at runtime.
 __init__() = Memento.register(LOGGER)
 
 include("io/common.jl")
