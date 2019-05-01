e97f0ac327de01c3d3d8845c5c0cf8fc15972690
Byron Tasseff <byron@tasseff.com>
2019-04-30 20:53:00 -0600

    Adding stub network expansion test.

--- test/ne.jl
+++ test/ne.jl
@@ -1,9 +1,10 @@
-@testset "Hazen-Williams MILP-R Network Expansion Problems" begin
-    #@testset "Shamir network (unknown flow directions)." begin
-    #    network = WaterModels.parse_file("../test/data/epanet/shamir.inp")
-    #    modifications = WaterModels.parse_file("../test/data/json/shamir.json")
-    #    InfrastructureModels.update_data!(network, modifications)
-    #    solution = run_ne(network, MILPRWaterModel, cbc, alpha=0.852)
-    #    @test solution["termination_status"] == MOI.OPTIMAL
-    #end
+@testset "Hazen-Williams MICP Network Expansion Problems" begin
+    @testset "Shamir network (unknown flow directions)." begin
+        network = WaterModels.parse_file("../test/data/epanet/shamir.inp")
+        modifications = WaterModels.parse_file("../test/data/json/shamir.json")
+        InfrastructureModels.update_data!(network, modifications)
+        solution = run_ne(network, MICPWaterModel, ipopt, alpha=1.852, relaxed=true)
+        @test solution["termination_status"] == MOI.ALMOST_LOCALLY_SOLVED ||
+              solution["termination_status"] == MOI.LOCALLY_SOLVED
+    end
 end
