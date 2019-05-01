f1a9bcc1a1580e6bf41328d2bb962bb6132f3945
Byron Tasseff <byron@tasseff.com>
2019-04-30 21:51:04 -0600

    Adding additional tests for helper functions.

---
+++ test/base.jl
@@ -0,0 +1,44 @@
+@testset "Helper Functions" begin
+    network_path = "../test/data/epanet/shamir.inp"
+    post_wf = WaterModels.get_post_wf(1.852)
+    wm = build_generic_model(network_path, CNLPWaterModel, post_wf)
+
+    @testset "ismultinetwork" begin
+        @test ismultinetwork(wm) == false
+    end
+
+    @testset "nw_ids" begin
+        @test all(nw_ids(wm) .== [0])
+    end
+
+    @testset "nws" begin
+        @test nws(wm) == wm.ref[:nw]
+    end
+
+    @testset "ids" begin
+        @test all(ids(wm, 0, :reservoirs) .== [1])
+        @test all(ids(wm, :reservoirs) .== [1])
+    end
+
+    @testset "ref" begin
+        @test ref(wm) == wm.ref[:nw][0]
+        @test ref(wm, 0) == wm.ref[:nw][0]
+        @test ref(wm, :reservoirs) == wm.ref[:nw][0][:reservoirs]
+        @test ref(wm, 0, :reservoirs) == wm.ref[:nw][0][:reservoirs]
+        @test ref(wm, :reservoirs, 1) == wm.ref[:nw][0][:reservoirs][1]
+        @test ref(wm, 0, :reservoirs, 1) == wm.ref[:nw][0][:reservoirs][1]
+        @test ref(wm, 0, :reservoirs, 1, "head") == 210.0
+    end
+
+    @testset "var" begin
+        @test var(wm, 0) == wm.var[:nw][0]
+        @test var(wm, 0, :q⁻) == wm.var[:nw][0][:q⁻]
+        @test var(wm, 0, :q⁻, 1) == wm.var[:nw][0][:q⁻][1]
+    end
+
+    @testset "con" begin
+        @test con(wm, 0) == wm.con[:nw][0]
+        @test con(wm, 0, :flow_conservation) == wm.con[:nw][0][:flow_conservation]
+        @test con(wm, 0, :flow_conservation, 2) == wm.con[:nw][0][:flow_conservation][2]
+    end
+end
