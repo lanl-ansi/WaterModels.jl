612c0ccfad3396ac3c94d663a53ad9fdb766e61f
ccoffrin <carleton@coffrin.com>
2019-07-23 17:04:43 -0600

    tidy problem definitions

--- src/prob/wf.jl
+++ src/prob/wf.jl
@@ -2,46 +2,46 @@ function run_wf(network, model_constructor, optimizer; relaxed::Bool=false, kwar
     return run_generic_model(network, model_constructor, optimizer, post_wf, relaxed=relaxed; kwargs...)
 end
 
-function post_wf(wm::GenericWaterModel{T}, n::Int=wm.cnw; kwargs...) where T
+function post_wf(wm::GenericWaterModel{T}) where T
     if T <: Union{AbstractMICPForm, AbstractNCNLPForm}
-        function_f_alpha(wm, n, convex=false)
+        function_f_alpha(wm, convex=false)
     elseif T <: AbstractCNLPForm
-        function_if_alpha(wm, n, convex=true)
+        function_if_alpha(wm, convex=true)
     end
 
-    variable_head(wm, n)
-    variable_flow(wm, n)
-    variable_pump(wm, n)
+    variable_head(wm)
+    variable_flow(wm)
+    variable_pump(wm)
 
-    for a in collect(ids(wm, n, :pipes))
-        constraint_potential_loss_pipe(wm, a, n)
-        constraint_link_flow(wm, a, n)
+    for a in collect(ids(wm, :pipes))
+        constraint_potential_loss_pipe(wm, a)
+        constraint_link_flow(wm, a)
     end
 
-    for a in collect(ids(wm, n, :pumps))
-        constraint_potential_loss_pump(wm, a, n)
+    for a in collect(ids(wm, :pumps))
+        constraint_potential_loss_pump(wm, a)
     end
 
-    for (i, junction) in ref(wm, n, :junctions)
-        constraint_flow_conservation(wm, i, n)
+    for (i, junction) in ref(wm, :junctions)
+        constraint_flow_conservation(wm, i)
 
         #if junction["demand"] > 0.0
-        #    constraint_sink_flow(wm, i, n)
+        #    constraint_sink_flow(wm, i)
         #end
     end
 
-    #for i in collect(ids(wm, n, :reservoirs))
-    #    constraint_source_flow(wm, i, n)
+    #for i in collect(ids(wm, :reservoirs))
+    #    constraint_source_flow(wm, i)
     #end
 
-    objective_wf(wm, n)
+    objective_wf(wm)
 end
 
 function run_mn_wf(file, model_constructor, optimizer; kwargs...)
     return run_generic_model(file, model_constructor, optimizer, post_mn_wf; multinetwork=true, kwargs...)
 end
 
-function post_mn_wf(wm::GenericWaterModel{T}; kwargs...) where T
+function post_mn_wf(wm::GenericWaterModel{T}) where T
     for (n, network) in nws(wm)
         if T <: Union{AbstractMICPForm, AbstractNCNLPForm}
             function_f_alpha(wm, n, convex=false)
