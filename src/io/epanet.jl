1f41a89b1f04c1a9d80c35f131625bda318f9f72
Byron Tasseff <btasseff@lanl.gov>
2019-07-24 18:38:34 -0600

    Removing extraneous comments.

--- src/io/epanet.jl
+++ src/io/epanet.jl
@@ -417,13 +417,6 @@ function _clean_nothing!(data)
     return data
 end
 
-#def clean_empty(d):
-#    if not isinstance(d, (dict, list)):
-#        return d
-#    if isinstance(d, list):
-#        return [v for v in (clean_empty(v) for v in d) if v]
-#    return {k: v for k, v in ((k, clean_empty(v)) for k, v in d.items()) if v}
-
 """
     parse_epanet(path)
 
