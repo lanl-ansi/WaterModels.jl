b94301b439b740a9d226d13478d940c453a1bdda
Byron Tasseff <btasseff@lanl.gov>
2019-07-31 15:40:24 -0600

    Adding van Zyl network for OWF testing... currently infeasible.

--- test/owf.jl
+++ test/owf.jl
@@ -1,18 +1,12 @@
 @testset "Optimal Water Flow Problems" begin
-    # Set test network paths.
-    richmond_skeleton_path = "../test/data/epanet/richmond-skeleton.inp"
-    richmond_skeleton_sp_path = "../test/data/epanet/richmond-skeleton-sp.inp"
+    van_zyl_path = "../test/data/epanet/van_zyl.inp"
 
-    @testset "Richmond network, single time point, NCNLP formulation." begin
-        solution = run_owf(richmond_skeleton_sp_path, NCNLPWaterModel, ipopt, relaxed=true)
-        #@test solution["termination_status"] == MOI.LOCALLY_SOLVED
-    end
-
-    @testset "Richmond network, multinetwork, NCNLP formulation." begin
-        data = WaterModels.parse_file(richmond_skeleton_path)
-        mn_data = WaterModels.make_multinetwork(data)
-        # TODO make this test work
-        #solution = run_mn_owf(mn_data, NCNLPWaterModel, ipopt, relaxed=true)
-        #@test solution["termination_status"] == MOI.LOCALLY_SOLVED
+    @testset "van Zyl network, multinetwork, NCNLP formulation." begin
+        network_data = WaterModels.parse_file(van_zyl_path)
+        mn_data = WaterModels.make_multinetwork(network_data)
+        wm = build_generic_model(mn_data, NCNLPWaterModel, WaterModels.post_mn_owf, multinetwork=true)
+        f = Juniper.register(fun(wm, :head_loss)..., autodiff=false)
+        juniper = JuMP.with_optimizer(Juniper.Optimizer, nl_solver=ipopt, registered_functions=[f], log_levels=[])
+        solution = WaterModels.solve_generic_model(wm, juniper)
     end
 end
