9e13fa30424a918695f2ded95874ac311c44ae30
Byron Tasseff <btasseff@lanl.gov>
2019-09-04 16:36:40 -0600

    Fixing a couple tests.

--- src/WaterModels.jl
+++ src/WaterModels.jl
@@ -10,21 +10,22 @@ import Memento
 import MathOptInterface
 const _MOI = MathOptInterface
 
-# Create our module level logger (this will get precompiled)
+# Create our module-level logger (this will get precompiled).
 const _LOGGER = Memento.getlogger(@__MODULE__)
 
-# Register the module level logger at runtime so that folks can access the logger via `getlogger(WaterModels)`
-# NOTE: If this line is not included then the precompiled `WaterModels._LOGGER` won't be registered at runtime.
+# Register the module-level logger at runtime so that folks can access the
+# logger via `getlogger(WaterModels)` NOTE: If this line is not included, then
+# the precompiled `WaterModels._LOGGER` will not be registered at runtime.
 __init__() = Memento.register(_LOGGER)
 
-"Suppresses information and warning messages output by WaterModels, for fine grained control use the Memento package."
+"Suppresses information and warning messages output by WaterModels. For fine-grained control, use the Memento package."
 function silence()
     Memento.info(_LOGGER, "Suppressing information and warning messages for the rest of this session. Use the Memento package for more fine-grained control of logging.")
     Memento.setlevel!(Memento.getlogger(InfrastructureModels), "error")
     Memento.setlevel!(Memento.getlogger(WaterModels), "error")
 end
 
-"alows the user to set the logging level without the need to add Memento"
+"Allows the user to set the logging level without the need to add Memento."
 function logger_config!(level)
     Memento.config!(Memento.getlogger("WaterModels"), level)
 end
