5bc732c13ede91cbf361265cde093ebb4b040b8e
Byron Tasseff <btasseff@lanl.gov>
2019-10-04 09:03:00 -0600

    Decrease number of breakpoints in tests.

--- test/ne.jl
+++ test/ne.jl
@@ -23,10 +23,10 @@
         data = WaterModels.parse_file("../test/data/epanet/shamir.inp")
         modifications = WaterModels.parse_file("../test/data/json/shamir.json")
         InfrastructureModels.update_data!(data, modifications)
-        wm = build_model(data, MILPWaterModel, WaterModels.post_ne, ext=Dict(:num_breakpoints => 5))
+        wm = build_model(data, MILPWaterModel, WaterModels.post_ne, ext=Dict(:num_breakpoints => 3))
         solution = WaterModels.optimize_model!(wm, cbc)
         @test solution["termination_status"] == OPTIMAL
-        @test isapprox(solution["objective"], 1.36e6, rtol=1.0e-4)
+        @test isapprox(solution["objective"], 4.71e5, rtol=1.0e-4)
     end
 
     #@testset "Shamir network (unknown flow directions), MILPR formulation." begin
