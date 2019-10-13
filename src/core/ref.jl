14470940ba3792a2efaa37a1c021bef035031215
Byron Tasseff <byron@tasseff.com>
2019-10-13 13:39:46 -0600

    Correcting flow bound calculation.

--- src/core/ref.jl
+++ src/core/ref.jl
@@ -118,8 +118,9 @@ function calc_flow_rate_bounds(wm::AbstractWaterModel, n::Int=wm.cnw)
 
             # TODO: These seem to be valid bounds when tanks and pumps aren't
             # present, but it should be fixed to include these, too.
-            lb[a][r_id] = sign(dh_lb[a]) * (abs(dh_lb[a]) / (L * r))^(inv(alpha))
-            ub[a][r_id] = sign(dh_ub[a]) * (abs(dh_ub[a]) / (L * r))^(inv(alpha))
+            lb[a][r_id] = sign(dh_lb[a]) * (abs(dh_lb[a]) / (L * r))^inv(alpha)
+            ub[a][r_id] = sign(dh_ub[a]) * (abs(dh_ub[a]) / (L * r))^inv(alpha)
+
             lb[a][r_id] = max(lb[a][r_id], -sum_demand)
             ub[a][r_id] = min(ub[a][r_id], sum_demand)
 
