f878d784aabfe5a5a3319ec650123a5a5af92f77
Devon SIgler <DevonSigler3@gmail.com>
2021-04-15 15:40:35 -0600

    for debugging time decomp script

--- src/prob/wf.jl
+++ src/prob/wf.jl
@@ -206,3 +206,112 @@ function build_mn_wf(wm::AbstractWaterModel)
     # Add the objective.
     objective_wf(wm)
 end
+
+function build_mn_wf_decomp(wm::AbstractWaterModel)
+    # Create head loss functions, if necessary.
+    _function_head_loss(wm)
+
+    network_ids = sort(collect(nw_ids(wm)))
+
+    #for (n, network) in nws(wm)
+    for n in network_ids[2:end]
+
+        # Physical variables.
+        variable_head(wm; nw=n)
+        variable_flow(wm; nw=n)
+        variable_pump_head_gain(wm; nw=n)
+        variable_pump_power(wm; nw=n)
+
+        # Indicator (status) variables.
+        variable_des_pipe_indicator(wm; nw=n)
+        variable_pump_indicator(wm; nw=n)
+        variable_regulator_indicator(wm; nw=n)
+        variable_valve_indicator(wm; nw=n)
+
+        # Create flow-related variables for node attachments.
+        variable_demand_flow(wm; nw=n)
+        variable_reservoir_flow(wm; nw=n)
+        variable_tank_flow(wm; nw=n)
+
+        # Flow conservation at all nodes.
+        for (i, node) in ref(wm, :node; nw=n)
+            constraint_flow_conservation(wm, i; nw=n)
+            constraint_node_directionality(wm, i; nw=n)
+        end
+
+        # Constraints on pipe flows, heads, and physics.
+        for (a, pipe) in ref(wm, :pipe; nw=n)
+            constraint_pipe_flow(wm, a; nw=n)
+            constraint_pipe_head(wm, a; nw=n)
+            constraint_pipe_head_loss(wm, a; nw=n)
+        end
+
+        # Constraints on design pipe flows, heads, and physics.
+        for (a, des_pipe) in ref(wm, :des_pipe; nw=n)
+            constraint_on_off_des_pipe_flow(wm, a; nw=n)
+            constraint_on_off_des_pipe_head(wm, a; nw=n)
+            constraint_on_off_des_pipe_head_loss(wm, a; nw=n)
+        end
+
+        # Selection of design pipes along unique arcs.
+        for (k, arc) in ref(wm, :des_pipe_arc; nw=n)
+            constraint_des_pipe_flow(wm, k, arc[1], arc[2]; nw=n)
+            constraint_des_pipe_head(wm, k, arc[1], arc[2]; nw=n)
+            constraint_des_pipe_selection(wm, k, arc[1], arc[2]; nw=n)
+        end
+
+        # Constraints on pump flows, heads, and physics.
+        for (a, pump) in ref(wm, :pump; nw=n)
+            constraint_on_off_pump_head(wm, a; nw=n)
+            constraint_on_off_pump_head_gain(wm, a; nw=n)
+            constraint_on_off_pump_flow(wm, a; nw=n)
+            constraint_on_off_pump_power(wm, a; nw=n)
+        end
+
+        # Constraints on short pipe flows and heads.
+        for (a, regulator) in ref(wm, :regulator; nw=n)
+            constraint_on_off_regulator_head(wm, a; nw=n)
+            constraint_on_off_regulator_flow(wm, a; nw=n)
+        end
+
+        # Constraints on short pipe flows and heads.
+        for (a, short_pipe) in ref(wm, :short_pipe; nw=n)
+            constraint_short_pipe_head(wm, a; nw=n)
+            constraint_short_pipe_flow(wm, a; nw=n)
+        end
+
+        # Constraints on valve flows and heads.
+        for (a, valve) in ref(wm, :valve; nw=n)
+            constraint_on_off_valve_head(wm, a; nw=n)
+            constraint_on_off_valve_flow(wm, a; nw=n)
+        end
+    end
+
+    # Get all network IDs in the multinetwork.
+    #network_ids = sort(collect(nw_ids(wm)))
+
+    # Start with the first network, representing the initial time step.
+    n_1 = network_ids[1]
+    variable_head(wm; nw= n_1)
+    variable_tank_flow(wm; nw=n_1)
+
+    # Constraints on tank volumes.
+    # for (i, tank) in ref(wm, :tank; nw = n_1)
+    #     # Set initial conditions of tanks.
+    #     constraint_tank_volume(wm, i; nw = n_1)
+    # end
+
+    # Constraints on tank volumes.
+    for n_2 in network_ids[2:end]
+        # Constrain tank volumes after the initial time step.
+        for (i, tank) in ref(wm, :tank; nw = n_2)
+            constraint_tank_volume(wm, i, n_1, n_2)
+        end
+
+        n_1 = n_2 # Update the first network used for integration.
+    end
+
+    # Add the objective.
+    objective_wf(wm)
+
+end
