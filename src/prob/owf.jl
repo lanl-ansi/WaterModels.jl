265e788ef86253731dc882027b039a85f10b0377
Byron Tasseff <btasseff@lanl.gov>
2021-09-14 11:02:59 -0600

    Add switching variant of mn_owf.

--- src/prob/owf.jl
+++ src/prob/owf.jl
@@ -46,3 +46,22 @@ function build_mn_owf(wm::AbstractWaterModel)
     # Add the optimal water flow objective.
     objective_owf(wm)
 end
+
+
+function build_mn_owf_switching(wm::AbstractWaterModel)
+    # Build the water flow problem.
+    build_mn_wf_switching(wm)
+
+    # Get all network IDs in the multinetwork.
+    network_ids = sort(collect(nw_ids(wm)))
+
+    # Ensure tanks recover their initial volume.
+    n_1, n_f = network_ids[1], network_ids[end]
+
+    for i in ids(wm, n_f, :tank)
+        constraint_tank_volume_recovery(wm, i, n_1, n_f)
+    end
+
+    # Add the optimal water flow objective.
+    objective_owf(wm)
+end
