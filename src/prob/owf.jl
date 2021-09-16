683e5e185aeaef59f49bf45211c822920d25a6b0
Byron Tasseff <btasseff@lanl.gov>
2021-09-16 08:41:58 -0600

    Add convenience functions for solving multinetwork OWF problems with switching.

--- src/prob/owf.jl
+++ src/prob/owf.jl
@@ -20,6 +20,17 @@ function run_mn_owf(network, model_constructor, optimizer; kwargs...)
 end
 
 
+function solve_mn_owf_switching(file, model_constructor, optimizer; kwargs...)
+    return solve_model(file, model_constructor, optimizer, build_mn_owf_switching; multinetwork=true, kwargs...)
+end
+
+
+function run_mn_owf_switching(network, model_constructor, optimizer; kwargs...)
+    Memento.warn(_LOGGER, "\"run_\" methods should be renamed \"solve_\" and will be deprecated in future versions.")
+    return solve_mn_owf(network, model_constructor, optimizer; kwargs...)
+end
+
+
 function build_owf(wm::AbstractWaterModel)
     # Build the water flow problem.
     build_wf(wm)
