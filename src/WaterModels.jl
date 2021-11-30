c0eb980ce9752c1b6739cfa39fc2b715decfee49
dsigler1234 <devonsigler3@gmail.com>
2021-11-30 16:34:49 -0700

    "fixing temporal decomp"

--- src/WaterModels.jl
+++ src/WaterModels.jl
@@ -100,6 +100,7 @@ include("util/variable_index.jl")
 include("util/bound_problem.jl")
 include("util/pairwise_cuts.jl")
 include("util/obbt.jl")
+include("util/temporal_decomp_utils.jl")
 
 # This must be included last to support automated export.
 include("core/export.jl")
