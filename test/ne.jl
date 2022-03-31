c7872aa61e24caeb40570e1f2482012f24d6218a
Byron Alexander Tasseff <btasseff@lanl.gov>
2022-03-30 18:15:44 -0600

    Add tests for `solve_ne` and `run_ne`.

--- test/ne.jl
+++ test/ne.jl
@@ -25,9 +25,7 @@ for formulation in [NCWaterModel, NCDWaterModel, CRDWaterModel, LAWaterModel, LR
         network["ne_short_pipe"]["1"]["status"] = WaterModels.STATUS_UNKNOWN
         network["ne_short_pipe"]["1"]["construction_cost"] = 10.0
         WaterModels.correct_network_data!(network)
-
         network_mn = WaterModels.make_multinetwork(network)
-        set_flow_partitions_si!(network_mn, 10.0, 1.0e-4)
 
         wm = instantiate_model(network_mn, formulation, build_mn_ne)
         result = WaterModels.optimize_model!(wm, optimizer = _choose_solver(wm, nlp_solver, milp_solver))
@@ -37,4 +35,34 @@ for formulation in [NCWaterModel, NCDWaterModel, CRDWaterModel, LAWaterModel, LR
         @test isapprox(result["solution"]["nw"]["2"]["ne_short_pipe"]["1"]["status"], 1.0, atol = 1.0e-3)
         @test isapprox(result["objective"], (length(result["solution"]["nw"]) - 1.0) * 10.0)
     end
+end
+
+@testset "solve_ne" begin
+    network = WaterModels.parse_file("../test/data/epanet/snapshot/pipe-hw-lps.inp"; skip_correct = true)
+    network["ne_short_pipe"]["1"] = pop!(network["pipe"], "1")
+
+    delete!.(Ref(network["ne_short_pipe"]["1"]), ["diameter", "length", "has_valve", "roughness"])
+    network["ne_short_pipe"]["1"]["status"] = WaterModels.STATUS_UNKNOWN
+    network["ne_short_pipe"]["1"]["construction_cost"] = 10.0
+    WaterModels.correct_network_data!(network)
+
+    result = WaterModels.solve_ne(network, LRDWaterModel, milp_solver)
+    result = WaterModels.run_ne(network, LRDWaterModel, milp_solver)
+    @test _is_valid_status(result["termination_status"])
+end
+
+
+@testset "solve_mn_ne" begin
+    network = WaterModels.parse_file("../test/data/epanet/multinetwork/pipe-hw-lps.inp"; skip_correct = true)
+    network["ne_short_pipe"]["1"] = pop!(network["pipe"], "1")
+
+    delete!.(Ref(network["ne_short_pipe"]["1"]), ["diameter", "length", "has_valve", "roughness"])
+    network["ne_short_pipe"]["1"]["status"] = WaterModels.STATUS_UNKNOWN
+    network["ne_short_pipe"]["1"]["construction_cost"] = 10.0
+    WaterModels.correct_network_data!(network)
+
+    network_mn = WaterModels.make_multinetwork(network)
+    result = WaterModels.solve_mn_ne(network_mn, LRDWaterModel, milp_solver)
+    result = WaterModels.run_mn_ne(network_mn, LRDWaterModel, milp_solver)
+    @test _is_valid_status(result["termination_status"])
 end
