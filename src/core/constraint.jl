ef09281776a6a2b417bd4ec3b6bc144786adb221
Byron Alexander Tasseff <btasseff@lanl.gov>
2022-11-10 06:33:42 -0700

    Add more documentation for common constraints.

--- src/core/constraint.jl
+++ src/core/constraint.jl
@@ -5,11 +5,34 @@
 
 """
     constraint_flow_conservation(
-        wm, n, i, pipe_fr, pipe_to, des_pipe_fr, des_pipe_to, pump_fr, pump_to,
-        regulator_fr, regulator_to, short_pipe_fr, short_pipe_to, valve_fr, valve_to,
-        reservoirs, tanks, dispatachable_demands, fixed_demand)
+        wm::AbstractWaterModel,
+        n::Int,
+        i::Int,
+        pipe_fr::Array{Int,1},
+        pipe_to::Array{Int,1},
+        des_pipe_fr::Array{Int,1},
+        des_pipe_to::Array{Int,1},
+        pump_fr::Array{Int,1},
+        pump_to::Array{Int,1},
+        regulator_fr::Array{Int,1},
+        regulator_to::Array{Int,1},
+        short_pipe_fr::Array{Int,1},
+        short_pipe_to::Array{Int,1},
+        valve_fr::Array{Int,1},
+        valve_to::Array{Int,1},
+        reservoirs::Array{Int,1},
+        tanks::Array{Int,1},
+        dispatchable_demands::Array{Int,1},
+        fixed_demand::Float64
+    )
 
-Adds a constraint that ensures flow conservation at a node in the network.
+Adds a constraint that ensures volumetric flow rate (and thus mass) is
+conserved at node `i` and subnetwork (or time) index `n` in the network. Here,
+`pipe_fr`, `pipe_to`, etc., are components that are directed _from_ or _to_
+node `i`, respectively. Additionally, `reservoirs`, `tanks`, and
+`dispatchable_demands` are node-attached components that variably contribute to
+nodal inflow and outflow at node `i`. Finally, `fixed_demand` is the fixed
+amount of flow demanded at node `i`, which may be negative _or_ nonnegative.
 """
 function constraint_flow_conservation(
     wm::AbstractWaterModel, n::Int, i::Int, pipe_fr::Array{Int,1},
@@ -42,11 +65,24 @@ end
 
 
 """
-    constraint_tank_volume_fixed(wm, n, i, V_0)
+    constraint_tank_volume_fixed(
+        wm::AbstractWaterModel,
+        n::Int,
+        i::Int,
+        V_0::Float64,
+        time_step::Float64,
+        min_vol::Float64,
+        max_vol::Float64
+    )
 
-Adds a constraint that ensures the volume of a tank at some time step is fixed. Here, `wm`
-is the WaterModels object, `n` is the index of a subnetwork within a multinetwork, `i` is
-the index of the tank, and `V_0` is the fixed volume of the tank that is desired.
+Adds a constraint that ensures the volume of a tank at some time index is fixed.
+Here, `wm` is the WaterModels object, `n` is the index of a subnetwork within a
+multinetwork, `i` is the index of the tank, and `V_0` is the fixed volume of
+the tank that is desired. Also adds constraints that ensure, after integration
+of volume forward in time, the new volume will reside between predefined lower
+and upper bounds. To that end, `time_step` is the time step between the current
+time index and the next, `min_vol` is the minimum volume of water that must be
+present in the tank, and `max_vol` is the maximum volume of water in the tank.
 """
 function constraint_tank_volume_fixed(wm::AbstractWaterModel, n::Int, i::Int, V_0::Float64, time_step::Float64, min_vol::Float64, max_vol::Float64)
     V, q = var(wm, n, :V, i), var(wm, n, :q_tank, i)
@@ -57,6 +93,23 @@ function constraint_tank_volume_fixed(wm::AbstractWaterModel, n::Int, i::Int, V_
 end
 
 
+"""
+    constraint_des_pipe_selection(
+        wm::AbstractWaterModel,
+        n::Int,
+        k::Int,
+        node_fr::Int,
+        node_to::Int,
+        des_pipes::Array{Int,1}
+    )
+
+Adds a constraint that ensures exactly one design pipe will be selected per arc
+that comprises one or more design pipe possibilities. Here, `wm` is the
+WaterModels object, `n` is the index of a subnetwork within a multinetwork, `k`
+is the index of the arc comprising multiple design pipes, `node_fr` and
+`node_to` are node indices that connect the arc, and `des_pipes` is the vector
+of design pipes that reside along the same arc `k`.
+"""
 function constraint_des_pipe_selection(wm::AbstractWaterModel, n::Int, k::Int, node_fr::Int, node_to::Int, des_pipes::Array{Int,1})
     z_des_pipe = var(wm, n, :z_des_pipe)
     c = JuMP.@constraint(wm.model, sum(z_des_pipe[a] for a in des_pipes) == 1.0)
@@ -65,15 +118,22 @@ end
 
 
 """
-    constraint_tank_volume(wm, n_1, n_2, i, time_step)
+    constraint_tank_volume(
+        wm::AbstractWaterModel,
+        n_1::Int,
+        n_2::Int,
+        i::Int,
+        time_step::Float64
+    )
 
-Adds a constraint that integrates the volume of a tank forward in time. Here, `wm` is the
-WaterModels object, `n_1` is the index of a subnetwork within a multinetwork, `n_2` is the
-index of another subnetwork forward in time, relative to `n_1`, i is the index of the tank,
-and time_step is the time step (in seconds) of the interval from network `n_1` to `n_2`.
+Adds a constraint that integrates the volume of a tank forward in time. Here,
+`wm` is the WaterModels object, `n_1` is the index of a subnetwork within a
+multinetwork, `n_2` is the index of another subnetwork forward in time,
+relative to `n_1`, i is the index of the tank, and `time_step` is the time step
+of the interval from network `n_1` to `n_2`.
 """
 function constraint_tank_volume(wm::AbstractWaterModel, n_1::Int, n_2::Int, i::Int, time_step::Float64)
-    q_tank = var(wm, n_1, :q_tank, i) # Tank outflow.
+    q_tank = var(wm, n_1, :q_tank, i) # Tank _outflow_.
     V_1, V_2 = var(wm, n_1, :V, i), var(wm, n_2, :V, i)
     c = JuMP.@constraint(wm.model, V_1 - V_2 == q_tank * time_step)
     append!(con(wm, n_2, :tank_volume)[i], [c])
@@ -81,14 +141,22 @@ function constraint_tank_volume(wm::AbstractWaterModel, n_1::Int, n_2::Int, i::I
 
 
 """
-    constraint_tank_volume_recovery(wm, i, n_1, n_f)
+    constraint_tank_volume_recovery(
+        wm::AbstractWaterModel,
+        i::Int,
+        n_1::Int,
+        n_f::Int
+    )
 
-Adds a constraint that ensures the volume of a tank at the end of the time horizon is
-greater than or equal to the volume of the tank at the beginning of the time horizon. Here,
-`wm` is the WaterModels object, `n_1` is the index of the first subnetwork within a
-multinetwork, `n_f` is the index of the final subnetwork, and i is the index of the tank.
+Adds a constraint that ensures the volume of a tank at the end of the time
+horizon is greater than or equal to the volume of the tank at the beginning
+of the time horizon. Here, `wm` is the WaterModels object, `i` is the index of
+the tank, `n_1` is the index of the first subnetwork within a multinetwork, and
+`n_f` is the index of the final subnetwork. Also updates the minimum head in
+the `ref` dictionary for node `i` at the final subnetwork index `n_f` to take
+the minimum head value at `n_1` if it is larger, as implied by this constraint.
 """
-function constraint_tank_volume_recovery(wm::AbstractWaterModel, i::Int, n_1::Int, n_f::Int)    
+function constraint_tank_volume_recovery(wm::AbstractWaterModel, i::Int, n_1::Int, n_f::Int)
     _initialize_con_dict(wm, :tank_volume_recovery, nw = n_f)
     V_1, V_f = var(wm, n_1, :V, i), var(wm, n_f, :V, i)
     c = JuMP.@constraint(wm.model, V_1 <= V_f)
@@ -101,6 +169,24 @@ function constraint_tank_volume_recovery(wm::AbstractWaterModel, i::Int, n_1::In
 end
 
 
+"""
+    constraint_on_off_pump_power_best_efficiency(
+        wm::AbstractWaterModel,
+        n::Int,
+        a::Int,
+        density::Float64,
+        gravity::Float64,
+        q_min_forward::Float64
+    )
+
+Adds a constraint that expresses pump power as a linear expression of flow,
+where coefficients of this linear expression are computed using an assumption
+that the pump will be operating at its best efficiency point. Here, `wm` is the
+WaterModels object, `n` is the index of the subnetwork within a multinetwork,
+`a` is the index of the pump, `density` is the density of water, `gravity` is
+acceleration due to gravity, and `q_min_forward` is the minimum amount of flow
+transported through the pump when it is active (and flow is directed forward).
+"""
 function constraint_on_off_pump_power_best_efficiency(
     wm::AbstractWaterModel, n::Int, a::Int,
     density::Float64, gravity::Float64, q_min_forward::Float64)
@@ -122,6 +208,24 @@ function constraint_on_off_pump_power_best_efficiency(
 end
 
 
+"""
+    constraint_on_off_pump_power_custom(
+        wm::AbstractWaterModel,
+        n::Int,
+        a::Int,
+        power_fixed::Float64,
+        power_variable::Float64
+    )
+
+Adds a constraint that expresses pump power as a linear expression of flow,
+where coefficients of this linear expression are given by `power_fixed` and
+`power_variable`. Here, `wm` is the WaterModels object, `n` is the index of the
+subnetwork within a multinetwork, `a` is the index of the pump, `power_fixed`
+is the amount of power consumed by the pump when it is active and transporting
+no flow (i.e., the point of intersection at zero flow on the vertical axis of a
+power-versus-flow curve), and `power_variable` is the amount of power consumed
+by the pump per unit flow.
+"""
 function constraint_on_off_pump_power_custom(wm::AbstractWaterModel, n::Int, a::Int, power_fixed::Float64, power_variable::Float64)
     # Gather pump flow, power, and status variables.
     q, P, z = var(wm, n, :q_pump, a), var(wm, n, :P_pump, a), var(wm, n, :z_pump, a)
@@ -134,6 +238,22 @@ function constraint_on_off_pump_power_custom(wm::AbstractWaterModel, n::Int, a::
 end
 
 
+"""
+    constraint_on_off_pump_group(
+        wm::AbstractWaterModel,
+        n::Int,
+        k::Int,
+        pump_indices::Set{Int}
+    )
+
+Adds a constraint that imposes symmetry-breaking lexicographic sorting of pump
+activation statuses on groups of identical pumps operating in parallel along
+the same arc of the network. Reduces the combinatorial complexity of problems
+involving multiple pumps operating in parallel. Here, `wm` is the WaterModels
+object, `n` is the index of the subnetwork within a multinetwork, `k` is the
+index of the arc that includes the group of identical pumps, and `pump_indices`
+is the set of pump indices for pumps operating in parallel along arc `k`.
+"""
 function constraint_on_off_pump_group(wm::AbstractWaterModel, n::Int, k::Int, pump_indices::Set{Int})
     pump_indices_sorted = sort(collect(pump_indices))
 
@@ -147,6 +267,17 @@ function constraint_on_off_pump_group(wm::AbstractWaterModel, n::Int, k::Int, pu
 end
 
 
+"""
+    constraint_on_off_pump_switch(
+        wm::AbstractWaterModel,
+        a::Int,
+        network_ids::Array{Int,1},
+        max_switches::Int
+    )
+
+Adds a constraint that limits the number of times a pump can be switched from
+off to on in a multiperiod pump scheduling problem.
+"""
 function constraint_on_off_pump_switch(wm::AbstractWaterModel, a::Int, network_ids::Array{Int, 1}, max_switches::Int)
     z_switch_on_sum = sum(var(wm, n, :z_switch_on_pump, a) for n in network_ids)
     c = JuMP.@constraint(wm.model, z_switch_on_sum <= max_switches)
